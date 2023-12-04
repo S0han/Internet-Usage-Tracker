15718a796e5e27ece9f7a9fdc83aecbe1b468dd5
Kane Pereira <kane.pereira18@gmail.com>
2023-12-03 21:55:03 -0500

    use localStorage to manage start time and perform check to start when tab open

--- popup.js
+++ popup.js
@@ -1,47 +1,55 @@
 //Apply stopwatch principles to code
-let startTime = localStorage.getItem('startTime') || new Date().getTime();          //get or set the start time
-let stopwatchInterval;                                                              //interval     
-let elapsedPausedTime = 0;                                                          //keep track of time while stopped
+let startTime = parseInt(localStorage.getItem('startTime'))|| new Date().getTime();             //get or set the start time
+let stopwatchInterval;                                                                          //interval     
+let elapsedPausedTime = 0;                                                                      //keep track of time while stopped
+
+function initAndStartStopwatch() {
+    if (!startTime) {                                                                           // If start time is not retrieved properly, set a default value
+        startTime = new Date().getTime();
+        localStorage.setItem('startTime', startTime);
+    }
+    startStopwatch();
+}
 
 function startStopwatch() {
     if (!stopwatchInterval) {
-        startTime = new Date().getTime() - elapsedPausedTime;                       //get the start time
-        stopwatchInterval = setInterval(udpateStopwatch, 1000);                     //get the updated time interval in seconds
+        startTime = new Date().getTime() - elapsedPausedTime;                                   //get the start time
+        stopwatchInterval = setInterval(udpateStopwatch, 1000);                                 //get the updated time interval in seconds
     }
 }
 
 function stopStopwatch() {
-    clearInterval(stopwatchInterval);                                               //stop the updates to the HTML timer display
-    elapsedPausedTime = new Date().getTime() - startTime;                           //calculate time elapsed since paused
-    stopwatchInterval = null;                                                       //reset the interval variable
+    clearInterval(stopwatchInterval);                                                           //stop the updates to the HTML timer display
+    elapsedPausedTime = new Date().getTime() - startTime;                                       //calculate time elapsed since paused
+    stopwatchInterval = null;                                                                   //reset the interval variable
 }
 
 //reset the timer function ----> event listener to check if new day occurs which triggers the reset
 function resetStopwatch() {
-    stopStopwatch();                                                                //stop the interval
-    elapsedPausedTime = 0;                                                          //reset variable
-    document.getElementById("stopwatch").innerHTML = "00:00:00";                    //reset the HTML display timer
+    stopStopwatch();                                                                            //stop the interval
+    elapsedPausedTime = 0;                                                                      //reset variable
+    document.getElementById("stopwatch").innerHTML = "00:00:00";                                //reset the HTML display timer
 }
 
 //take the current global time in milliseconds and translate it into updating HTML stopwatch
 function udpateStopwatch() {
-    let currentTime = new Date().getTime();                                         //get current time in milliseconds
-    let elapsedTime = currentTime - startTime;                                      //calculate time elapsed since the start
-    let seconds = Math.floor(elapsedTime / 1000) % 60;                              //get the seconds
-    let minutes = Math.floor(elapsedTime / 1000 / 60) % 60;                         //get the minutes
-    let hours = Math.floor(elapsedTime / 1000 / 60/ 60);                            //get the hours
-    let displayTime = pad(hours) + ":" + pad(minutes) + ":" + pad(seconds);         //format the display time
+    let currentTime = new Date().getTime();                                                     //get current time in milliseconds
+    let elapsedTime = currentTime - startTime;                                                  //calculate time elapsed since the start
+    let seconds = Math.floor(elapsedTime / 1000) % 60;                                          //get the seconds
+    let minutes = Math.floor(elapsedTime / 1000 / 60) % 60;                                     //get the minutes
+    let hours = Math.floor(elapsedTime / 1000 / 60/ 60);                                        //get the hours
+    let displayTime = pad(hours) + ":" + pad(minutes) + ":" + pad(seconds);                     //format the display time
     
     let currentDayStart = new Date();
-    currentDayStart.setHours(0, 0, 0, 0);                                            //set this time to start of day
+    currentDayStart.setHours(0, 0, 0, 0);                                                       //set this time to start of day
     if (new Date(startTime).getTime() < currentDayStart.getTime()) {
         resetStopwatch();
-        startTime = new Date().getTime();                                           //update the start time for the new day
-        localStorage.setItem("startTime", startTime);                               //store the new start time
+        startTime = new Date().getTime();                                                       //update the start time for the new day
+        localStorage.setItem("startTime", startTime);                                           //store the new start time
         startStopwatch();
     }
     
-    document.getElementById("stopwatch").innerHTML = displayTime;                   //update the HTML to show new display time **RESET**
+    document.getElementById("stopwatch").innerHTML = displayTime;                               //update the HTML to show new display time **RESET**
 }
 
 //check if the section of the timmer hh/mm/ss are abover or below 10 to see whether or not to add a leading 0 to keep format
@@ -60,8 +68,8 @@ function isSameDay(a, b) {
 //checks to see if a specific URL is opened which will trigger a timer.    **START**
 chrome.webNavigation.onDOMContentLoaded.addListener(function(tab) {
     if(tab.frameId === 0 && tab.url.includes("instagram.com")) {
-        console.log("Youtube has been loaded...starting timer");                    //upload notification to console ---> can be deleted after testing
-        startStopwatch();                                                           //start timing usage
+        console.log("Youtube has been loaded...starting timer");                                //upload notification to console ---> can be deleted after testing
+        initAndStartStopwatch();                                                                //start timing usage
     }
 });
 
@@ -69,7 +77,7 @@ chrome.webNavigation.onDOMContentLoaded.addListener(function(tab) {
 document.addEventListener('DOMContentLoaded', function () {
     const redirButton = document.getElementById('backPageRedirect');
     redirButton.addEventListener('click', function () {
-        chrome.tabs.create({url: chrome.runtime.getURL('backpage.html')});          //this is the site where the reports are stored
+        chrome.tabs.create({url: chrome.runtime.getURL('backpage.html')});                      //this is the site where the reports are stored
     });
 });
 
@@ -77,9 +85,10 @@ document.addEventListener('DOMContentLoaded', function () {
 chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
     console.log("Tab with ID " + tabId + " was closed");
     chrome.tabs.get(tabId, function(tab) {
-        if (tab && tab.url.includes("instagram.com")) {                             //this is where we define the site we want to monitor
-            console.log("tab with URL " + tab.url + " was closed.")                 //stop timing usage
-            stopStopwatch();
+        if (tab && tab.url.includes("instagram.com")) {                                         //this is where we define the site we want to monitor
+            console.log("tab with URL " + tab.url + " was closed.")                             //stop timing usage
+            stopStopwatch();                                                                    //stop the stop watch      
+            localStorage.setItem('startTime', startTime);                                       //store the current start time to track where it left off
         }
     });
 });
