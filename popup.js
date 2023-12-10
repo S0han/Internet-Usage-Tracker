34c7c275a68cc52db0bdd1204530bda274e68f9b
Kane Pereira <kane.pereira18@gmail.com>
2023-12-09 23:44:40 -0500

    check for popup visibility

--- popup.js
+++ popup.js
@@ -1,9 +1,10 @@
 let startTime;
 let stopwatchInterval;
+let isTabVisible = true; // Track tab visibility
 
 function startStopwatch() {
     if (!stopwatchInterval) {
-        startTime = startTime || new Date().getTime();
+        startTime = new Date().getTime();
         stopwatchInterval = setInterval(updateStopwatch, 1000);
     }
 }
@@ -14,23 +15,42 @@ function stopStopwatch() {
 }
 
 function updateStopwatch() {
-    let currentTime = new Date().getTime();
-    let elapsedTime = (currentTime - startTime) / 1000;
+    if (isTabVisible) {
+        let currentTime = new Date().getTime();
+        let elapsedTime = (currentTime - startTime) / 1000;
 
-    let seconds = Math.floor(elapsedTime % 60);
-    let minutes = Math.floor(elapsedTime / 60) % 60;
-    let hours = Math.floor(elapsedTime / 3600);
+        let seconds = Math.floor(elapsedTime % 60);
+        let minutes = Math.floor(elapsedTime / 60) % 60;
+        let hours = Math.floor(elapsedTime / 3600);
 
-    let displayTime = pad(hours) + ":" + pad(minutes) + ":" + pad(seconds);
-    document.getElementById("stopwatch").innerHTML = displayTime;
+        let displayTime = pad(hours) + ":" + pad(minutes) + ":" + pad(seconds);
+        document.getElementById("stopwatch").innerHTML = displayTime; // Update the display
+    }
 }
 
 function pad(number) {
     return (number < 10 ? "0" : "") + number;
 }
 
-// Start timer immediately when page loads
-startStopwatch();
+function handleVisibilityChange() {
+    isTabVisible = !document.hidden;
+    if (!isTabVisible) {
+        stopStopwatch();
+    }
+}
 
-// Stop timer when tab is closed/left
-window.addEventListener('beforeunload', stopStopwatch);
+document.addEventListener('visibilitychange', handleVisibilityChange);
+
+// Start timer immediately when the specified tab (YouTube) page loads
+chrome.webNavigation.onDOMContentLoaded.addListener(function (details) {
+    if (details.frameId === 0 && details.url.includes("youtube.com")) {
+        startStopwatch();
+    }
+});
+
+// Listen for tab closure or navigation
+chrome.tabs.onRemoved.addListener(function (tabId, removeInfo) {
+    if (removeInfo.isWindowClosing && !isTabVisible && removeInfo.url.includes("youtube.com")) {
+        stopStopwatch();
+    }
+});
