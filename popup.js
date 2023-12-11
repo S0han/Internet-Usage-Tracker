c1e57b9ab457dfe5deee41fdb7ecd7e8141998c2
Kane Pereira <kane.pereira18@gmail.com>
2023-12-11 01:24:56 -0500

    send message to popup.js to manipulate DOM

--- popup.js
+++ popup.js
@@ -1,30 +1,6 @@
-let startTime;
-let stopwatchInterval;
-
-function startStopwatch() {
-    if (!stopwatchInterval) {
-        startTime = new Date().getTime();
-        stopwatchInterval = setInterval(updateStopwatch, 1000);
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    if (message.action === "updateTimer") {
+        // Update the timer displayed in the extension's popup window
+        document.getElementById("stopwatch").innerHTML = message.timerValue;
     }
-}
-
-function stopStopwatch() {
-    clearInterval(stopwatchInterval);
-    stopwatchInterval = null;
-}
-
-function updateStopwatch() {
-    let currentTime = new Date().getTime();
-    let elapsedTime = (currentTime - startTime) / 1000;
-
-    let seconds = Math.floor(elapsedTime % 60);
-    let minutes = Math.floor(elapsedTime / 60) % 60;
-    let hours = Math.floor(elapsedTime / 3600);
-
-    let displayTime = pad(hours) + ":" + pad(minutes) + ":" + pad(seconds);
-    document.getElementById("stopwatch").innerHTML = displayTime; // Update the display
-}
-
-function pad(number) {
-    return (number < 10 ? "0" : "") + number;
-}
+});
--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,40 @@
+let startTime;
+let stopwatchInterval;
+
+function startStopwatch() {
+    if (!stopwatchInterval) {
+        startTime = new Date().getTime();
+        stopwatchInterval = setInterval(updateStopwatch, 1000);
+    }
+}
+
+function stopStopwatch() {
+    clearInterval(stopwatchInterval);
+    stopwatchInterval = null;
+}
+
+function updateStopwatch() {
+    let currentTime = new Date().getTime();
+    let elapsedTime = (currentTime - startTime) / 1000;
+
+    let seconds = Math.floor(elapsedTime % 60);
+    let minutes = Math.floor(elapsedTime / 60) % 60;
+    let hours = Math.floor(elapsedTime / 3600);
+
+    let displayTime = pad(hours) + ":" + pad(minutes) + ":" + pad(seconds);
+
+    // Send a message to the content script with the updated timer value
+    chrome.tabs.query({ url: "*://*.youtube.com/*" }, function(tabs) {
+        tabs.forEach(tab => {
+            chrome.tabs.sendMessage(tab.id, { action: "updateTimer", timerValue: displayTime });
+        });
+    });
+}
+
+function pad(number) {
+    return (number < 10 ? "0" : "") + number;
+}
+
 // Start timer immediately when the specified tab (YouTube) page loads
 chrome.webNavigation.onDOMContentLoaded.addListener(function (details) {
     if (details.frameId === 0 && details.url.includes("youtube.com")) {
